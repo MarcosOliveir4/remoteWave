e4015144e7b222d6ca64e890c3126043c85e13c6
MarcosOliveir4 <marcosassis071@gmail.com>
2026-03-03 09:46:12 -0300

    feat: add CI workflow and coverage configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      reporter: 'lcov',
+      cleanOnRerun: true,
+    },
     watch: false,
     environment: 'happy-dom', // Specify happy-dom as the DOM environment
     setupFiles: ['./vitest-setup.ts'], // Optional: for setting up @testing-library/jest-dom matchers
